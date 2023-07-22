9a8a0b65a239101727ac69fb6334533acc4bd635
ATTOLOU <129006552+regisatl@users.noreply.github.com>
2023-07-22 21:51:09 +0100

    Projet Angular of App Music

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { AlbumDetailsComponent } from './album-details/album-details.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { AlbumDetailsComponent } from './album-details/album-details.component';
     AppComponent,
     AlbumsComponent,
     AlbumDetailsComponent,
+    SearchComponent,
     
   ],
   imports: [
