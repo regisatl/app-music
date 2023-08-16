29afea3e8fdc6fd5af91e03e6ce06ae9aa3190a9
regisatl <regisattolou21@outlook.fr>
2023-08-16 15:42:49 +0100

    Projet Angular of App Music

--- src/app/admin/add-album/add-album.component.ts
+++ src/app/admin/add-album/add-album.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Album } from 'src/app/album';
 import { fadeInAnimation } from 'src/app/animation.module';
 import { AlbumService } from '../../album.service';
@@ -16,11 +16,13 @@ export class AddAlbumComponent implements OnInit {
       albumForm!: FormGroup; // Utilisez FormGroup ici
       album!: Album; // Ajoutez cette propriété
       albumId!: string;
+      msgSuccess: string | undefined = 'Album ajouté avec succès';
 
       constructor(
             private formBuilder: FormBuilder,
             private albumService: AlbumService,
             private router: Router,
+            private route: ActivatedRoute,
       ) { }
 
 
@@ -33,7 +35,8 @@ export class AddAlbumComponent implements OnInit {
                   ref: ['', // 20FTX
                         [
                               Validators.required,
-                              Validators.pattern('\\w{3}') // doit avoir 5 caractères 
+                              Validators.minLength(3)
+                              //Validators.pattern('\\w{5}') // doit avoir 5 caractères 
                               //Validators.pattern('[A-Z0-9]{5}')
                         ]
                   ],
@@ -50,6 +53,11 @@ export class AddAlbumComponent implements OnInit {
                               Validators.max(900)
                         ]
                   ],
+
+                  tags: this.formBuilder.array([
+                        this.formBuilder.control('')
+                  ]),
+
                   status: 'off',
             });
 
@@ -60,23 +68,33 @@ export class AddAlbumComponent implements OnInit {
       get title() { return this.albumForm.get("title"); }
       get description() { return this.albumForm.get("description"); }
       get duration() { return this.albumForm.get("duration"); }
+      get tags() { return this.albumForm.get("tags") as FormArray; }
+
+      onInit()  {
+            this.route.queryParams.subscribe(params => {
+                  this.msgSuccess = params["message"];
+            })
+      }
 
       onSubmit() {
             if (this.albumForm.valid) {
                   const newAlbum: Album = this.albumForm.value;
-                  // Appelez la méthode d'ajout d'album dans le service
+                  //Appelez la méthode d'ajout d'album dans le service
                   this.albumService.addAlbum(newAlbum)
                         .subscribe(
                               () => {
-                                    alert('Album ajouté avec succès');
                                     // Redirigez vers une autre page après l'ajout
-                                    this.router.navigate(['/admin'], { queryParams: { message: 'success' } });
+                                    this.router.navigate(['/admin'], { queryParams: { message: 'Album ajouté avec success' } });
                               },
                               error => {
                                     console.warn("Erreur lors de l'ajout de l'album :", error);
                               }
                         );
             }
+      }
+
+      addTag() {
+            this.tags.push(this.formBuilder.control(''))
       }
 
 
--- src/app/admin/album/album.component.ts
+++ src/app/admin/album/album.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Album } from 'src/app/album';
 import { AlbumService } from 'src/app/album.service';
 import { fadeInAnimation } from 'src/app/animation.module';
@@ -12,18 +14,62 @@ import { fadeInAnimation } from 'src/app/animation.module';
 export class AlbumComponent implements OnInit {
 
       albums!: Album[];
+      messageSuccess: string | undefined = 'Album ajouté avec succès';
+      albumId!: string;
+      albumForm!: FormGroup;
 
       constructor(
+            private formBuilder: FormBuilder,
+            private route: ActivatedRoute,
+            private router: Router,
             private albumService: AlbumService
       ) { }
 
       ngOnInit(): void {
             this.albumService
-            .paginate(0, this.albumService.paginateNumberPage())
-           .subscribe({
-                  next: (alb: Album[]) => { this.albums = alb }
+                  .paginate(0, this.albumService.paginateNumberPage())
+                  .subscribe({
+                        next: (alb: Album[]) => { this.albums = alb }
+                  });
+
+            this.route.queryParams.subscribe(params => {
+                  this.messageSuccess = params["message"];
             });
-            
+
+            this.albumId = this.route.snapshot.params['albumId'];
+
+            this.albumService.getAlbum(this.albumId)?.subscribe(album => {
+                  this.albumForm = this.formBuilder.group({
+                        name: [
+                              album.name,
+                              [
+                                    Validators.required,
+                                    Validators.minLength(5)
+                              ]
+                        ],
+                        ref: [
+                              album.ref,
+                              [
+                                    Validators.required
+                              ]
+                        ],
+                        title: [
+                              album.title,
+                              [
+                                    Validators.required
+                              ]
+                        ],
+                        description: [
+                              album.description],
+                        duration: [
+                              album.duration,
+                              [
+                                    Validators.required
+                              ]
+                        ],
+                  });
+            });
+
       }
 
       onSetPaginate($event: { start: number, end: number }) {
@@ -33,6 +79,21 @@ export class AlbumComponent implements OnInit {
                   .subscribe({
                         next: (alb: Album[]) => this.albums = alb
                   });
+      }
+
+      onDelete() {
+            if (confirm('Êtes-vous sûr de vouloir supprimer cet album ?')) {
+                  this.albumService.deleteAlbum(this.albumId).subscribe(
+                        () => {
+                              alert('Album supprimé avec succès');
+                              // Redirigez vers une autre page après la suppression
+                              this.router.navigate(['/admin'], { queryParams: { message: 'success' } });
+                        },
+                        error => {
+                              console.error("Erreur lors de la suppression de l'album :", error);
+                        }
+                  );
+            }
       }
 
 
